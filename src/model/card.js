12f4f8ba58d9c9b6c38598d659719e152572a516
siderilla <siderilla11@gmail.com>
2025-03-16 00:20:36 +0100

    i prefer pokemon

---
+++ src/model/card.js
@@ -0,0 +1,29 @@
+export default class Card {
+	constructor(id, name, typeline, type, desc, race, atk, def, level, attribute, card_image, card_prices = [])
+}
+
+"id": 11714098,
+        "name": "30,000-Year White Turtle",
+        "typeline": [
+            "Aqua",
+            "Normal"
+        ],
+        "type": "Normal Monster",
+        "humanReadableCardType": "Normal Monster",
+        "frameType": "normal",
+        "desc": "''A huge turtle that has existed for over 30,000 years.''",
+        "race": "Aqua",
+        "atk": 1250,
+        "def": 2100,
+        "level": 5,
+        "attribute": "WATER",
+        "ygoprodeck_url": "https://ygoprodeck.com/card/30-000-year-white-turtle-1012",
+        "card_images": [
+            {
+                "id": 11714098,
+                "image_url": "https://images.ygoprodeck.com/images/cards/11714098.jpg",
+                "image_url_small": "https://images.ygoprodeck.com/images/cards_small/11714098.jpg",
+                "image_url_cropped": "https://images.ygoprodeck.com/images/cards_cropped/11714098.jpg"
+            }
+        ],
+        "card_prices": [
